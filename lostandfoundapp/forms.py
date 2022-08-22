c852663fd4435e391d3bf82b44c87a1c41202ad9
adiletexe <108264958+adiletexe@users.noreply.github.com>
2022-08-22 15:15:11 +0600

    styling pages part9210327814

--- lostandfoundapp/forms.py
+++ lostandfoundapp/forms.py
@@ -2,6 +2,7 @@ from django.forms import ModelForm
 from .models import FoundItems
 
 class CreationForm(ModelForm):
+    title = 
     class Meta:
         model = FoundItems
         fields = ['title', 'textfield', 'image', 'category', 'phonenumber', 'email']
